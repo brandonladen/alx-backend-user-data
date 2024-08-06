88d8eff3cfba87b7fbf548fa283b819f2ccd06f2
brandonladen <brandonladen486@gmail.com>
2024-08-06 21:11:44 +0300

    added shebang in all files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Module for authentication in a Flask application.
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Module for Basic authentication in a Flask application.
 
