514de172484f98cd4080e55fec7dc7000ebd62e3
brandonladen <brandonladen486@gmail.com>
2024-08-07 17:18:08 +0300

    creating a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """Session authentication module for the API.
 """
 from .auth import Auth
+import uuid
 
 class SessionAuth (Auth):
     """
     class SessionAuth that inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id
+        """
+        if not user_id:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
