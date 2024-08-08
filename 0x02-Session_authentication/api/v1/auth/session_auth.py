4879774d687a7e99b9201114f71c62efeb36a6e4
brandonladen <brandonladen486@gmail.com>
2024-08-08 22:12:32 +0300

    updates session_auth file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,7 +25,7 @@ class SessionAuth (Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """
+        """returns session id
         """
         if not session_id:
             return None
