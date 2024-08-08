cb303f86a576ab09d06f7428658e642ad126c339
brandonladen <brandonladen486@gmail.com>
2024-08-08 10:57:43 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,5 +19,15 @@ class SessionAuth (Auth):
             return None
         
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
+        self.__class__.user_id_by_session_id[session_id] = user_id
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        """
+        if not session_id:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        
+        return self.__class__.user_id_by_session_id.get(session_id)
